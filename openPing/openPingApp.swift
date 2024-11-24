c862b190abd65c60dcd61fc29b25667081f66f83
Mark Miranda <weatherby@m2-MBA>
2024-11-24 09:36:30 -0700

    Initial Commit

---
+++ openPing/openPingApp.swift
@@ -0,0 +1,17 @@
+//
+//  openPingApp.swift
+//  openPing
+//
+//  Created by Mark Miranda on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct openPingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ openPingTests/openPingTests.swift
@@ -0,0 +1,17 @@
+//
+//  openPingTests.swift
+//  openPingTests
+//
+//  Created by Mark Miranda on 11/24/24.
+//
+
+import Testing
+@testable import openPing
+
+struct openPingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
